9a3bf2290188e967d876761356f2f776364caf05
Salil Desai <salil@dhcp-hol-2319.redrover.cornell.edu>
2019-10-25 17:05:56 -0400

    added pokedex with species, types, and base stats

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -7,18 +7,26 @@ public class Pokemon {
 	public ArrayList<Integer> pp; /** pp[i] is the current pp of moves[i] **/
 	public int level, maxHp, atk, def, spc, spe, currHp;
 	
+	//TODO: status effects ex. sleep, substitute
+	//TODO: stat modifications ex. atk down, def up
 	
 	/**
 	 * Construct a new pokemon with the specified species and moves
-	 * Uses JSON to get base stats and types, and uses the Move objects
+	 * Uses Pokedex JSON to get base stats and types, and uses the Move objects
 	 * to get move pp
 	 * 
 	 */
 	public Pokemon(String species, ArrayList<Move> moves, int level) {
+		this.species = species;
+		this.moves = moves;
+		this.pp = new ArrayList<Integer>();
+		for(Move m : this.moves) {
+			pp.add(m.maxPP);
+		}
+		this.level = level;
+		
+		// Load Types, Base Stats, 
 		// TODO
+		
 	}
-	
-	
-	//TODO: status effects ex. sleep, substitute
-	//TODO: stat modifications ex. atk down, def up
 }
