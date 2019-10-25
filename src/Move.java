399c581f9a42c4d09e1d6bcc240c7975324bcdd2
Salil Desai <salil@dhcp-hol-2319.redrover.cornell.edu>
2019-10-25 16:26:51 -0400

    Implemented 'use' function for moves which only checks for accuracy and deals damage, ignoring statuses, stat modifications, and secondary effects

--- src/Move.java
+++ src/Move.java
@@ -1,11 +1,79 @@
 
 public class Move {
 	public String name;
-	public int maxPP, power, accuracy;
+	public int maxPP, power;
+	public int accuracy; /** Base accuracy of a move, or -1 if it doesn't check for accuracy */
 	public Type type;
 	
-	public void attack(Pokemon user, Pokemon target) {
+	// TODO: Add status/stat modification/secondary effect
+	
+	/**
+	 * Have [user] attempt to use [this] on a [target]
+	 * "attempt to use" means behave as if the user had selected the move, and it is 
+	 * their turn in the speed bracket to try to attack, without considering or modifying pp
+	 * i.e
+	 * - Check for any status that would prevent the move's execution (not currently implemented)
+	 * - 	e.x. sleep, paralysis, protect
+	 * - Check if the move connected with target (by checking accuracy/evasion modifiers and base accuracy
+	 * If the move connected
+	 * - Update both pokemon's statuses and hp as necessary, considering base damage and secondary effects
+	 */
+	public void use(Pokemon user, Pokemon target) {
 		
+		// TODO: Check for status conditions that would prevent the move from being executed
+		
+		// Accuracy Check
+		// TODO: Check user and target accuracy modifications
+		if((accuracy != -1 && (Math.random() * 100) > accuracy) || (Math.random() < 1.0/256)) {
+			// The attack missed
+			// Any attack has a 1/256 chance of missing due to gen 1 glitch, independently of normal accuracy checks
+			return;
+		}
+		
+		int damage = damageDealt(user, target);
+		
+		target.currHp = Math.max(0, target.currHp - damage);
+		
+		// TODO: Check for secondary effects
+		
+	}
+	
+	/**
+	 * Returns amount of damage dealt to target assuming [this] lands successfully
+	 * when used by [user] on [target]
+	 * 
+	 * Uses the following formula: https://bulbapedia.bulbagarden.net/wiki/Damage#Damage_calculation
+	 */
+	public int damageDealt(Pokemon user, Pokemon target) {
+		boolean critical = (Math.random() < 0.5);
+		
+		int level = user.level * (critical ? 2 : 1);
+		int power = this.power;
+		
+		int attackingStat, defendingStat;
+		if(this.type.isPhysical()) {
+			attackingStat = user.atk;
+			defendingStat = target.def;
+		}
+		else {
+			attackingStat = user.spc;
+			defendingStat = target.spc;
+		}
+		
+		int baseDamage = (((((((2 * level)/5) + 2) * power * attackingStat)/(defendingStat))/50) + 2);
+		
+		// Random damage range
+		double modifier = ((int)(Math.random() * 39) + 217)/(255);
+		// STAB
+		for(Type t : user.types)
+			if(t == this.type)
+				modifier *= 1.5;
+		// Type effectiveness
+		for(Type t : target.types) {
+			modifier *= this.type.effectiveness(t);
+		}
+		
+		return (int)(baseDamage * modifier);
 	}
 	
 	//TODO: status + stat modifications
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -4,8 +4,8 @@ public class Pokemon {
 	public String species;
 	public ArrayList<Type> types;
 	public ArrayList<Move> moves;
+	public ArrayList<Integer> pp; /** pp[i] is the current pp of moves[i] **/
 	public int level, maxHp, atk, def, spc, spe, currHp;
-	
 	
 	
 	//TODO: status effects ex. sleep, substitute
