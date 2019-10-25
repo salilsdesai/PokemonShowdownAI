f78c4c272c7d804b7995671cda936d92d1a7f215
Salil Desai <salil@dhcp-hol-2319.redrover.cornell.edu>
2019-10-25 16:36:24 -0400

    added framework for Pokemon constructor

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -8,6 +8,17 @@ public class Pokemon {
 	public int level, maxHp, atk, def, spc, spe, currHp;
 	
 	
+	/**
+	 * Construct a new pokemon with the specified species and moves
+	 * Uses JSON to get base stats and types, and uses the Move objects
+	 * to get move pp
+	 * 
+	 */
+	public Pokemon(String species, ArrayList<Move> moves) {
+		// TODO
+	}
+	
+	
 	//TODO: status effects ex. sleep, substitute
 	//TODO: stat modifications ex. atk down, def up
 }
