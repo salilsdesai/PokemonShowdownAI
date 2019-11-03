7a249e639fa86eaabe5f146f518393276ccab9de
ayz9 <ayz9@cornell.edu>
2019-11-03 14:51:48 -0500

    Changed implementation of game state to use hashmap, instead of using indices to pair pokemon with their moves.

--- src/GameState.java
+++ src/GameState.java
@@ -1,34 +1,26 @@
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class GameState {
     /* Representation of player-one's team of pokemon and movesets. */
     public Team p1_team;
-    /* List of pokemon player one knows the opponent has and their statuses. The
-     * moves of the pokemon in this list are purely random and are meaningless.
-     * Moves which player-one knows player-two's pokemon have are stored in [playerTwoMoves].
-     */
-    public ArrayList<Pokemon> p2_pokemon;
-    /* List of movesets which player-one knows for player-two's pokemon. Since the
-     * game begins with unknown teams and movesets, this list is initially filled with
-     * empty sets. If the i'th pokemon on player-two's uses a move never seen before,
-     * this move is added to [playerTwoMoves[i]]. */
-    public HashSet<Move>[] p2_moves;
+    /* List of pokemon player-one knows the opponent has and their statuses. The
+     * moves of the pokemon stored in the key are purely random and meaningless.
+     * The value of each key stores the moves which player-one knows player-two's
+     * pokemon can perform. If player-one has not seen a specific pokemon use a move,
+     * then the value in the map will be an empty list. */
+    public HashMap<Pokemon, ArrayList<Move>> p2_pokemon;
 
     /* Player-two's active pokemon. */
     public static Pokemon p2_active;
 
     /* Default game state which describes the game as it begins. */
     public GameState(Team p1_team, Pokemon p2_active) {
+	/* Initialize variables. */
+	p2_pokemon = new HashMap<>();
 	/* Set up the current field. */
 	this.p1_team = p1_team;
 	this.p2_active = p2_active;
-	p2_pokemon.add(p2_active);
-	/* Initialize the knowledge known about the opponents pokemon. */
-	p2_moves = new HashSet[6];
-	for (int i = 0; i < p2_moves.length; i++) {
-	    p2_moves[i] = new HashSet<>();
-	}
+	p2_pokemon.put(p2_active, new ArrayList<>());
     }
-    
 }
