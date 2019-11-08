cefb4cc7a43fcfac314369961708cb31407118c7
ayz9 <ayz9@cornell.edu>
2019-11-08 15:58:19 -0500

    Fixed code formatting in gamestate

--- src/GameState.java
+++ src/GameState.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -9,7 +8,7 @@ public class GameState {
      * moves of the pokemon stored in the key are purely random and meaningless.
      * The value of each key stores the moves which player-one knows player-two's
      * pokemon can perform. If player-one has not seen a specific pokemon use a move,
-     * then the value in the map will be an empty list. */
+     * then the value in the map will be an empty set. */
     public HashMap<Pokemon, HashSet<Move>> p2_pokemon;
 
     /* Player-two's active pokemon. */
@@ -17,22 +16,22 @@ public class GameState {
 
     /* Default game state which describes the game as it begins. */
     public GameState(Team p1_team, Pokemon p2_active) {
-	/* Initialize variables. */
-	p2_pokemon = new HashMap<>();
-	/* Set up the current field. */
-	this.p1_team = p1_team;
-	this.p2_active = p2_active;
-	p2_pokemon.put(p2_active, new HashSet<>());
+    	/* Initialize variables. */
+    	p2_pokemon = new HashMap<>();
+    	/* Set up the current field. */
+    	this.p1_team = p1_team;
+    	this.p2_active = p2_active;
+    	p2_pokemon.put(p2_active, new HashSet<>());
     }
 
     /* Pass a deep copy of [p2_pokemon]. Intended to be used by successor game state. */
     public HashMap<Pokemon, HashSet<Move>> pass_on() {
 	HashMap<Pokemon, HashSet<Move>> ret = new HashMap<>();
-	// For every key, re-insert the key,value into the new hash map.
-	for (Pokemon p : p2_pokemon.keySet()) {
-	    // TODO: It might be helpful to clone the pokemon.
-	    ret.put(p, p2_pokemon.get(p));
-	}
+		// For every key, re-insert the key,value into the new hash map.
+		for (Pokemon p : p2_pokemon.keySet()) {
+			// TODO: It might be helpful to clone the pokemon.
+			ret.put(p, p2_pokemon.get(p));
+		}
 
 	return ret;
     }
