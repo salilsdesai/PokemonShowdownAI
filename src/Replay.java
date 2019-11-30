6027d15119d0e366a17b26891c022dfefa1aa805
Salil Desai <salilsdesai@gmail.com>
2019-11-30 13:09:54 -0500

    for parsing gamestate from replay, initialization and full framework is done, all that's left is to update the game state based on read actions

--- src/Replay.java
+++ src/Replay.java
@@ -4,12 +4,39 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * A class that stores a list of gamestates and the winner
- * of a particular Pokemon Showdown Gen 1 Random Battle Replay
+ * A class that stores a single randomly chosen gamestate from 
+ * the perspective of p1 and and the winner from a particular 
+ * Pokemon Showdown Gen 1 Random Battle Replay
  */
 public class Replay {
 	
-	public ArrayList<GameState> states;
+	public static class ReplaySwitchAction {
+		/** true if p1, false if p2 */
+		public boolean player;
+		/** the species of the pokemon being switched in */
+		public String species;
+		/** the level of the pokemon being switched in */
+		public int level;
+		/**
+		 * [line] is text of a switch line in a replay file,
+		 * in the form "|switch|p#a: ...|..., L##|##\/##"
+		 * @param line
+		 */
+		public ReplaySwitchAction(String line) {
+			int playerIndex = line.indexOf('p') + 1;
+			int speciesIndex = line.indexOf('|', playerIndex) + 1;
+			int commaIndex = line.indexOf(',', speciesIndex);
+			int endOfLevelIndex = line.indexOf('|', commaIndex);
+			player = (line.charAt(playerIndex) == '1');
+			species = line.substring(speciesIndex, commaIndex).toLowerCase();
+			level = Integer.parseInt(line.substring(commaIndex+3, endOfLevelIndex));
+		}
+		public String toString() {
+			return "Player " + (player ? '1' : '2') + " switch in " + species + " (" + level + ")";
+		}
+	}
+	
+	public GameState state;
 	/** true if p1 won, false if p2 won */
 	public boolean winner; 
 	
@@ -33,6 +60,21 @@ public class Replay {
 		}
 	}
 	
+	/**
+	 * Return the number of turns a particular replay lasted for
+	 * [replayLines] are the lines parsed from the html replay file
+	 */
+	public static int numTurns(String[] replayLines) {
+		int i = 0;
+		while(replayLines[i].length() < 2 || replayLines[i].charAt(1) != 'w') {
+			i++;
+		}
+		while(replayLines[i].length() < 2 || replayLines[i].charAt(1) != 't') {
+			i--;
+		}
+		return Integer.parseInt(replayLines[i].substring(replayLines[i].lastIndexOf('|') + 1, replayLines[i].length()));
+	}
+	
 	
 	/**
 	 * Parse a Replay from the provided file
@@ -43,17 +85,87 @@ public class Replay {
 		String[] lines = readFile(filename);
 		
 		int i = 0;
-		while(i < lines.length && !lines[i].equals("|start")) {
+		while(lines[i].length() < 12 || !lines[i].substring(0,11).equals("|player|p1|")) {
+			i++;
+		}
+		String p1Name = lines[i].substring(11, lines[i].indexOf('|', 11));
+		
+		while(!lines[i].equals("|start")) {
+			i++;
+		}
+				
+		ReplaySwitchAction p1StartAction = new ReplaySwitchAction(lines[i+1]);
+		ReplaySwitchAction p2StartAction = new ReplaySwitchAction(lines[i+2]);
+		
+		Pokemon p1StartPokemon = new Pokemon(p1StartAction.species, new String[4], p1StartAction.level);
+		Pokemon p2StartPokemon = new Pokemon(p2StartAction.species, new String[4], p2StartAction.level);
+		
+		ArrayList<Pokemon> p1StartTeamList = new ArrayList<Pokemon>();
+		p1StartTeamList.add(p1StartPokemon);
+		Team p1StartTeam = new Team(p1StartTeamList);
+		
+		state = new GameState(p1StartTeam, p2StartPokemon);
+		
+		i += 3;
+		
+		int numTurns = numTurns(lines);
+		int targetTurnNum = (int)(Math.random() * numTurns + 1);
+		int currTurn = 1;
+		
+		/*
+		 * Now we have initial game state ready, go turn by turn, 
+		 * adding a new game state each time, until we reach our target turn
+		 */
+		
+		while(i < lines[i].length() && currTurn < targetTurnNum) {
+			int secondBarIndex = lines[i].indexOf('|', 1);
+			if(lines[i].length() >= 2 && secondBarIndex != -1) {
+				String actionCategory = lines[i].substring(1, secondBarIndex);
+				if(actionCategory.equals("switch")) {
+					// TODO
+				}
+				else if (actionCategory.equals("move")) {
+					// TODO
+				}
+				else if (actionCategory.equals("faint")) {
+					// TODO
+				}
+				else if (actionCategory.equals("win")) {
+					// TODO
+				}
+				else if (actionCategory.equals("turn")) {
+					// TODO
+				}
+				else if (actionCategory.equals("-damage")) {
+					// TODO
+				}
+				else if (actionCategory.equals("-status")) {
+					// TODO
+				}
+				else if (actionCategory.equals("-faint")) {
+					// TODO
+				}
+				else if (actionCategory.equals("-unboost")) {
+					// TODO
+				}
+			}
 			i++;
 		}
 		
-		// TODO: Actually convert to gamestate
+		// Determine winner
+		while(lines[i].length() < 2 || lines[i].charAt(1) != 'w') {
+			i++;
+		}
+		String winnerName = lines[i].substring(lines[i].lastIndexOf('|') + 1, lines[i].length());
 		
+		System.out.println(winnerName);
+		System.out.println(p1Name);
 		
+		winner = (winnerName.equals(p1Name));
 	}
 	
 	public static void main(String[] args) {
-		Replay r = new Replay("Gen1RandomBattle-2019-11-30-sloworno-47olg10.html");
+		Replay r = new Replay("Gen1RandomBattle-2019-11-30-sloworno-47olg10.html");	
 	}
 	
 }
